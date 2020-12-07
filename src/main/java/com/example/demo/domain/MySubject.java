d25a83474cba92092b644c9480fc0c327c2f7132
halo-xh <xh1005@outlook.com>
2020-12-07 22:41:07 +0800

    init1207-1

--- src/main/java/com/example/demo/domain/MySubject.java
+++ src/main/java/com/example/demo/domain/MySubject.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.domain;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;

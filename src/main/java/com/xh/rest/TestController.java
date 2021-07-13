ca765d46526d272700f43ca5f502be7ac5aa711b
halo-xh <xh1005@outlook.com>
2021-07-13 20:37:50 +0800

    test

--- src/main/java/com/xh/rest/TestController.java
+++ src/main/java/com/xh/rest/TestController.java
@@ -1,5 +1,6 @@
 package com.xh.rest;
 
+import com.xh.common.AnonymousAccess;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,12 @@ public class TestController {
     
     @PostMapping("/test")
     public String testp(){
+        return "test post successfully";
+    }
+
+    @AnonymousAccess
+    @PostMapping("/test/asd")
+    public String testpasd(){
         return "test post successfully";
     }
     
